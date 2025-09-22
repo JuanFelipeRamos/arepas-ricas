33e6b6c4bd612ea71e59f99288da57d52655bded
Juan Felipe <jr9385197@gmail.com>
2025-09-21 23:37:00 -0500

    Creación del proyecto vue

---
+++ vue-project/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+</script>
+
+
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+
+<style>
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body, html {
+  width: 100%;
+  height: 100%;
+  background-color: white;
+}
+
+</style>
---
+++ vue-project/src/views/Home.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+
+<template>
+    <div class="container">
+        <h1>Arepas ricas</h1>
+    </div>
+</template>
+
+
+<style>
+
+</style>
