2135327689565468bc5027e7cf9966e2d8889135
Juan Felipe <jr9385197@gmail.com>
2025-09-21 23:59:19 -0500

    Instalación de librería para íconos

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -2,7 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'; //Librería para íconos
+
 const app = createApp(App)
+app.component('font-awesome-icon', FontAwesomeIcon);
 
 app.use(router)
 
